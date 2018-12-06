56a6b4b16c4b8bb8fb615778cc906a15c23874a8
Sara Vieira <hey@iamsaravieira.com>
2018-12-06 23:12:08 +0100

    fix mobile

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export default async p => {
   const url = 'https://api.npms.io/v2/package'
 
-  const f = await fetch(`${url}/${p}`)
+  const f = await fetch(`${url}/${p.towLowerCase().trim()}`)
   const rsp = await f.json()
 
   return rsp
