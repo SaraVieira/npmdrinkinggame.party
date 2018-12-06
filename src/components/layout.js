7dfa2149fce2ecfe6cca143198ace15e6b41d9fc
Sara Vieira <hey@iamsaravieira.com>
2018-12-06 23:08:29 +0100

    add meta tags0

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,14 +18,29 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
+        <Helmet title={data.site.siteMetadata.title}>
           <html lang="en" />
+          <meta charset="utf-8" />
+          <title>The NPM drinking game</title>
+          <meta name="description" content="Good Luck!" />
+          <meta
+            name="image"
+            content="https://rawcdn.githack.com/SaraVieira/npmdrinkinggame.com/bc29d1b751533b73c639654f13feb6592a52acc2/src/images/beer.png"
+          />
+          <meta itemprop="name" content="The NPM drinking game" />
+          <meta itemprop="description" content="Good Luck!" />
+          <meta
+            itemprop="image"
+            content="https://rawcdn.githack.com/SaraVieira/npmdrinkinggame.com/bc29d1b751533b73c639654f13feb6592a52acc2/src/images/beer.png"
+          />
+          <meta name="og:title" content="The NPM drinking game" />
+          <meta name="og:description" content="Good Luck!" />
+          <meta
+            name="og:image"
+            content="https://rawcdn.githack.com/SaraVieira/npmdrinkinggame.com/bc29d1b751533b73c639654f13feb6592a52acc2/src/images/beer.png"
+          />
+          <meta name="og:site_name" content="The NPM drinking game" />
+          <meta name="og:type" content="website" />
         </Helmet>
 
         {children}
